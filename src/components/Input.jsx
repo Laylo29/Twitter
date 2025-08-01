a39f2ea4a35b12b012d286aba4aea07e4173ed84
Laylo29 <ismatullayevalaylo6@gmail.com>
2025-08-01 10:52:52 +0500

    update commit

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,9 +1,7 @@
 
 
-const Input = ({classList}) => {
-  if(classList == "placeholder")
-  return 
-    <input className={` ${classList}outline-none w-[450px] h-[70px] mt-[36px] border-[#00000033] justify-center border-[1px] p-2  rounded-md`}>{title}</input>
+const Input = ({classList, placeholder, type}) => {
+  return <input type={type} className={`${classList} w-[450px] h-[70px] border-[#00000033] border-2  rounded-md px-[20px]`} placeholder={placeholder}/>
   
 }
 
--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -1,4 +1,4 @@
-import { Heading, Button, Text} from '../../components'
+import { Heading, Button, Text, Input} from '../../components'
 import {Logo} from '../../assets/images'
 
 const Login = () => {
@@ -7,9 +7,9 @@ const Login = () => {
     <div className='w-[450px] mx-auto mt-[137px]' > 
       <img className='pb-[36px]' src={Logo} alt="img" width={50} height={41}/>
       <Heading type={"h1"} title={"Log in to Twitter"} />
-      <form>
-         <input className="outline-none w-[450px] h-[70px] mt-[36px] border-[#00000033] justify-center border-[1px] p-2  rounded-md" autoComplete="off" required type="text" placeholder="Phone number, email address" name="todo" />
-         <input className="outline-none w-[450px] h-[70px] mt-[36px] mb-[25px] border-[#00000033] justify-center border-[1px] p-2  rounded-md" autoComplete="off" required type="text" placeholder="Phone number, email address" name="todo" />
+      <form className='pt-[36px] flex flex-col gap-[25px]'>
+       <Input type={'text'} placeholder={'Login'}/>
+       <Input type={'password'} placeholder={'Password'}/>
       <Button title={"Log In"} />
       <div className='flex justify-between mt-[40px]'>
            <Text title={"Forgot password?"}/>
